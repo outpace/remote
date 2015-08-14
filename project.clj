bcde4d4598daf0082b42d529c56bcb4490e686de
Jake McCrary <jake@jakemccrary.com>
2015-08-14 14:53:21 -0500

    Starting development on com.outpace/remote 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.outpace/remote "0.3.2"
+(defproject com.outpace/remote "0.3.3-SNAPSHOT"
   :description "A Clojure(Script) library and DSL for building client to remote service APIs."
 
   :url "http://github.com/outpace/remote"
